be4a99f58bdb9b089b4d1fc76b3b828ee1183846
Muhammad Abrar <abrar.at.hm@gmail.com>
2026-06-25 15:44:09 +0200

    Updated Type_Hints

--- 02 Type_Hints.py
+++ 02 Type_Hints.py
@@ -34,3 +34,44 @@ def greet(name):        # Normal function
 
 def greet(name:str):    # Type Hinted   (it is read as "name should be a string")
     print(name)
+
+
+
+# Return type Hints
+
+def square(n):                  # normal function(not Hinted)
+    return n*n
+
+def square(n: int) -> int:      # the arrow means that This function returns as an integer
+    return n*n
+
+
+
+# Lists
+
+number=[1,2,3]                  # normal
+
+from typing import list
+number: list[int]=[1,2,3]       # Hinted --> # it is read as "A list containing Integers" means list of integers
+
+
+
+# Tuple
+
+name_age=("Abrar",30)                       # normal
+
+from typing import Tuple
+name_age: Tuple[str,int]=("Abrar",30)       # Hinted
+
+#  it is read as "Tuple containing string, Integer"
+
+
+
+# Dictionaries
+
+score={"Abrar":"84"}                        # Normal 
+
+from typing import Dict
+score:dict[str,int]={"Abrar":90,"Ali":90}   # Hinted
+
+# it is read as "Dictionary whose keys are strings and values are integers"
