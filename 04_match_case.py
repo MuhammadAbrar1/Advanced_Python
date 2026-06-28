74ab836117b11b1f614f9fb44abf87113c3240aa
Muhammad Abrar <abrar.at.hm@gmail.com>
2026-06-28 20:38:07 +0200

    Practiced match case basics

---
+++ 04_match_case.py
@@ -0,0 +1,26 @@
+# choice=input("Enter choice: ")
+
+# match choice:
+#     case "1":
+#         print("Add student")
+#     case "2":
+#         print("Remove Student")
+#     case "3":
+#         print("View Student")
+#     case _:
+#         print("Invalid Choice")
+
+
+while True:
+    weather=input("Enter weather: ")
+    match weather:
+        case "cold":
+            print("wear jacket")
+        case "sunny":
+            print("wear sunglasses")
+        case "rainy":
+            print("take umbrella")
+        case "hot":
+            print("drik more water")
+        case _:
+            print("wrong entry")
